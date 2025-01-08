668cf71bbb3675ed0c7e12aecd0a08c128ba5b5d
shariful8523 <mdsharifulislam8523@gmail.com>
2025-01-08 16:17:36 +0600

    first commit

---
+++ index.js
@@ -0,0 +1,74 @@
+const express = require('express');
+const cors = require('cors');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
+require('dotenv').config(); 
+const app = express();
+const port = process.env.PORT || 3000;
+
+
+
+
+
+
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.wfpeu.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
+
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  }
+});
+
+async function run() {
+  try {
+    // Connect the client to the server	(optional starting in v4.7)
+    await client.connect();
+    // Send a ping to confirm a successful connection
+    await client.db("admin").command({ ping: 1 });
+    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+
+     // tutors related apis 
+
+    const tutorsCollection = client.db('SkillSync').collection('SkillSync');
+
+    app.get('/tutors', async(req, res) =>{
+        const cursor = tutorsCollection.find();
+        const result = await cursor.toArray();
+        res.send(result);
+    });
+
+    app.get('/tutors/:id', async(req, res) =>{
+      const id = req.params.id;
+      const query = {_id: new ObjectId(id)}
+      const result = await tutorsCollection.findOne(query);
+      res.send(result);
+    })
+
+
+
+
+
+
+
+  } finally {
+    // Ensures that the client will close when you finish/error
+    // await client.close();
+  }
+}
+run().catch(console.dir);
+
+
+
+app.use(cors());
+app.use(express.json());
+
+app.get('/', (req, res) => {
+    res.send(' server is runing')
+})
+
+app.listen(port, () =>{
+    console.log(`server is waiting at : ${port}`)
+})
